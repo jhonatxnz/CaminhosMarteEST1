8ba1d35c5b6720960588eaecf7e9ca336115bc83
Jhon <jw.jhonatan1711@gmail.com>
2022-06-07 22:06:10 -0300

    ai ai o txt errado

---
+++ apAgenda/FrmCaminhos.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace apCaminhos
+{
+    public partial class FrmCaminhos : Form
+    {
+        ListaDupla<Cidade> cidades;     //instanciamos a classe para utilizar seus metódos
+        ListaDupla<Ligacao> caminhos;   //instanciamos a classe para utilizar seus metódo
+        public FrmCaminhos()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmAgenda_Load(object sender, EventArgs e)
+        {
+            int indice = 0;
+            Caminhos.ImageList = imlBotoes;
+            foreach (ToolStripItem item in Caminhos.Items)
+                if (item is ToolStripButton)
+                    (item as ToolStripButton).ImageIndex = indice++;
+            cidades = new ListaDupla<Cidade>();
+            if (dlgAbrir.ShowDialog() == DialogResult.OK)
+            {
+                cidades.LerDados(dlgAbrir.FileName);    //le os dados do arquivo que o usuario escolheu
+                cidades.ExibirDados(cbxOrigem);         //exibe  os dados nos cbxDestino
+                cidades.ExibirDados(cbxDestino);        //exibe  os dados nos cbxDestino
+            }
+            caminhos = new ListaDupla<Ligacao>();
+            if (dlgAbrir.ShowDialog() == DialogResult.OK)
+            {
+                caminhos.LerDados(dlgAbrir.FileName);    //le os dados do arquivo que o usuario escolheu
+            }
+        }
+
+        private void pbMarte_Paint(object sender, PaintEventArgs e)
+        {
+            Font font = new Font("Arial", 10); //define a font como Arial,tamanho 10 (utilizado para passar de parâmetro no e.Graphics.DrawString)
+
+            cidades.PosicionarNoPrimeiro(); //posiciona o atua no primeiro
+            while (cidades.DadoAtual() != null)//percorre a lista
+            {
+                Cidade cidade = cidades.DadoAtual();
+                int x = (int)(cidade.X * pbMarte.Width); //multiplica o valor de X pela largura do pictureBox
+                int y = (int)(cidade.Y * pbMarte.Height);//multiplica o valor de Y pela altura do pictureBox
+                e.Graphics.FillEllipse(Brushes.Black, new Rectangle(x, y, 10, 10)); //pinta X e Y no pictureBox
+                e.Graphics.DrawString(cidade.Nome, font, Brushes.Black, x, y + 10);//escreve o nome da cidade no pictureBox(y + 10 para o nome da cidade não ficar em cima da bolinha)
+                //udX.Text = x.ToString();
+                //udY.Text = y.ToString();
+                cidades.AvancarPosicao(); //avança posicão
+            }
+            cidades.PosicionarNoPrimeiro();// posiciona no primeiro novamente por conta de um erro que tivemos zz
+        }
+    }
+}
---
+++ apAgenda/Ligacao.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+internal class Ligacao : IComparable<Ligacao>, IRegistro<Ligacao>
+{
+    const int tamCodigoOri = 3,
+              tamCodigoDest = 3,
+              tamDistancia = 5,
+              tamTempo = 4,
+              tamCusto = 5;
+
+    const int iniCodigoOrigem = 0,
+              iniCodigoDestino = iniCodigoOrigem + tamCodigoOri,
+              iniDistancia = iniCodigoDestino + tamCodigoDest,
+              iniTempo = iniDistancia + tamDistancia,
+              iniCusto = iniTempo + tamTempo;
+
+
+    string idCidadeOrigem, idCidadeDestino;
+    int distancia, tempo, custo;
+
+    public Ligacao(string idCidadeOrigem, string idCidadeDestino, int distancia, int tempo, int custo)
+    {
+        this.idCidadeOrigem = idCidadeOrigem;
+        this.idCidadeDestino = idCidadeDestino;
+        this.distancia = distancia;
+        this.tempo = tempo;
+        this.custo = custo;
+    }
+    public Ligacao() { }
+    public string IdCidadeOrigem { get => idCidadeOrigem; set => idCidadeOrigem = value; }
+    public string IdCidadeDestino { get => idCidadeDestino; set => idCidadeDestino = value; }
+    public int Distancia { get => distancia; set => distancia = value; }
+    public int Tempo { get => tempo; set => tempo = value; }
+    public int Custo { get => custo; set => custo = value; }
+
+    public int CompareTo(Ligacao outro)
+    {
+        return (idCidadeOrigem.ToUpperInvariant() + idCidadeDestino.ToUpperInvariant()).CompareTo(
+                outro.idCidadeOrigem.ToUpperInvariant() + outro.idCidadeDestino.ToUpperInvariant());
+    }
+
+    public Ligacao LerRegistro(StreamReader arquivo)
+    {
+        if (arquivo != null) // arquivo aberto?
+        {
+            string linha = arquivo.ReadLine();
+            IdCidadeOrigem = linha.Substring(iniCodigoOrigem, tamCodigoOri);
+            IdCidadeDestino = linha.Substring(iniCodigoDestino, tamCodigoDest);
+            Distancia = int.Parse(linha.Substring(iniDistancia, tamDistancia));
+            Tempo = int.Parse(linha.Substring(iniTempo, tamTempo));
+            Custo = int.Parse(linha.Substring(iniCusto, tamCusto));
+            return this; // retorna o próprio objeto Contato, com os dados
+        }
+        return default(Ligacao);
+    }
+    public void GravarRegistro(StreamWriter arq)
+    {
+        if (arq != null)  // arquivo de saída aberto?
+        {
+            arq.WriteLine(ParaArquivo());
+        }
+    }
+    public string ParaArquivo()
+    {
+        return $"{IdCidadeOrigem}{IdCidadeDestino}{Distancia:00000}{Tempo:0000}{Custo:00000}";
+    }
+
+    public override string ToString()
+    {
+        return $"{IdCidadeOrigem} {IdCidadeDestino} {Distancia:00000} {Tempo:0000} {Custo:00000}";
+    }
+}
